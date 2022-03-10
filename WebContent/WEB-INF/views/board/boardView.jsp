a77b7c873682521fd328ad7cca8807649835d42d
graysoft <graysoft@DESKTOP-SC816N1>
2022-03-10 12:28:33 +0900

    first commit

---
+++ WebContent/WEB-INF/views/board/boardView.jsp
@@ -0,0 +1,130 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<%@include file="/WEB-INF/views/common/common.jsp"%>    
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<title>boardView</title>
+	<script type="text/javascript">
+		//게시판 글 수정
+		$j(document).ready(function(){
+			
+			$j("#updateBtn").on("click",function(){
+				if($j('#updateBtn').val() == "확인"){
+					var uConfirm = confirm("수정하시겠습니까?");
+					if(uConfirm == true){
+						var $frm = $j('.boardUpdate :input');
+						var param = $frm.serialize();
+						
+						$j.ajax({
+						    url : "/board/boardUpadateAction.do",
+						    dataType: "json",
+						    type: "POST",
+						    data : param,
+						    success: function(data, textStatus, jqXHR)
+						    {
+								$j('#boardTitle').html('${board.boardTitle}');
+								$j('#boardComment').html('${board.boardComment}');
+								$j('#updateBtn').val("수정");	
+								alert("수정완료");
+								location.reload();
+						    },
+						    error: function (jqXHR, textStatus, errorThrown)
+						    {
+						    	alert("실패");
+						    }
+						});
+					}else{
+						location.reload();
+					}
+				}else{
+					$j('#boardTitle').html('<input name="boardTitle" type="text" size="50" value="${board.boardTitle}">');
+					$j('#boardComment').html('<textarea name="boardComment"  rows="20" cols="55">${board.boardComment}</textarea>');
+					$j('#updateBtn').val("확인");	
+				}
+			}); //개시판 글 수정
+
+			//게시판 글 삭제
+			$j('#deleteBtn').on("click", function(){
+				var deleteConfirm = confirm('삭제하시겠습니까?');
+				if(deleteConfirm == true){
+					var params = {
+							boardNum : $j('#boardNum').val() 
+						};
+					
+					$j.ajax({
+						url : "/board/boardDeleteAction.do",
+						type : "POST",
+						data : params,
+					    success: function(data, textStatus, jqXHR)
+					    {
+							alert("삭제완료");
+							
+							location.href = "/board/boardList.do";
+					    },
+					    error: function (jqXHR, textStatus, errorThrown)
+					    {
+					    	alert("실패");
+					    }
+					});
+				}
+
+			});		//게시판 글 삭제
+			
+		});//document ready
+	</script>
+	
+</head>
+<body class = "boardUpdate">
+<table align="center">
+		<tr>
+			<td align="right">
+					<input id = "updateBtn" type="button" value="수정"> <!-- 게시판 수정 버튼 -->
+					<input id = "deleteBtn" type="button" value="삭제"> <!-- 게시판 삭제  버튼-->
+					<input id = "boardNum" name = "boardNum" type="hidden" value="${boardNum}">
+			</td>
+			
+		</tr>
+
+	<tr>
+		<td>
+			<table border ="1">
+				<tr>
+					<td width="120" align="center">
+					Title
+					</td>
+					<td width="400">
+					<span id = "boardTitle">
+					${board.boardTitle}
+					</span>
+					</td>
+				</tr>
+				<tr>
+					<td height="300" align="center">
+					Comment
+					</td>
+					<td>
+					<span id = "boardComment">
+					${board.boardComment}
+					</span>
+					</td>
+				</tr>
+				<tr>
+					<td align="center">
+					Writer
+					</td>
+					<td>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+	<tr>
+		<td align="right">
+			<a href="/board/boardList.do">List</a>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
---
+++ src/com/spring/board/controller/BoardController.java
@@ -0,0 +1,137 @@
+package com.spring.board.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.board.HomeController;
+import com.spring.board.service.boardService;
+import com.spring.board.vo.BoardVo;
+import com.spring.board.vo.PageVo;
+import com.spring.common.CommonUtil;
+
+@Controller
+public class BoardController {
+	
+	@Autowired 
+	boardService boardService;
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/board/boardList.do", method = RequestMethod.GET)
+	public String boardList(Locale locale, Model model,PageVo pageVo) throws Exception{
+		
+		List<BoardVo> boardList = new ArrayList<BoardVo>();
+		
+		int page = 1;
+		int totalCnt = 0;
+		
+		if(pageVo.getPageNo() == 0){
+			pageVo.setPageNo(page);
+		}
+		
+		boardList = boardService.SelectBoardList(pageVo);
+		totalCnt = boardService.selectBoardCnt();
+		
+		model.addAttribute("boardList", boardList);
+		model.addAttribute("totalCnt", totalCnt);
+		model.addAttribute("pageNo", page);
+		return "board/boardList";
+	}
+	
+	@RequestMapping(value = "/board/{boardType}/{boardNum}/boardView.do", method = RequestMethod.GET)
+	public String boardView(Locale locale, Model model
+			,@PathVariable("boardType")String boardType
+			,@PathVariable("boardNum")int boardNum) throws Exception{
+		
+		BoardVo boardVo = new BoardVo();
+		
+		
+		boardVo = boardService.selectBoard(boardType,boardNum);
+		
+		model.addAttribute("boardType", boardType);
+		model.addAttribute("boardNum", boardNum);
+		model.addAttribute("board", boardVo);
+		
+		return "board/boardView";
+	}
+	
+	@RequestMapping(value = "/board/boardWrite.do", method = RequestMethod.GET)
+	public String boardWrite(Locale locale, Model model) throws Exception{
+		
+		
+		return "board/boardWrite";
+	}
+	
+	@RequestMapping(value = "/board/boardWriteAction.do", method = RequestMethod.POST)
+	@ResponseBody
+	public String boardWriteAction(Locale locale,BoardVo boardVo) throws Exception{
+		
+		HashMap<String, String> result = new HashMap<String, String>();
+		CommonUtil commonUtil = new CommonUtil();
+		
+		System.out.println(boardVo.getBoardTitle());
+		System.out.println(boardVo.getBoardComment());
+		
+		
+		int resultCnt = boardService.boardInsert(boardVo);
+		result.put("success", (resultCnt > 0)?"Y":"N");
+		String callbackMsg = commonUtil.getJsonCallBackString(" ",result);
+		
+		System.out.println("callbackMsg::"+callbackMsg);
+		
+		return callbackMsg;
+	}
+	
+	//게시판 삭제 메서드
+	@RequestMapping(value = "/board/boardDeleteAction.do", method = RequestMethod.POST)
+	@ResponseBody
+	public String Delete(BoardVo boardVO) throws Exception{
+		
+		HashMap<String, String> result = new HashMap<String, String>();
+		CommonUtil commonUtil = new CommonUtil();
+		
+		int boardNum = boardVO.getBoardNum();
+		int deleteRes = boardService.boardDelete(boardNum);
+		
+		result.put("success", (deleteRes > 0)?"Y":"N");
+		String callbackMsg = commonUtil.getJsonCallBackString(" ",result);
+		
+		System.out.println("callbackMsg::"+callbackMsg);
+		return callbackMsg;
+	}
+	
+	//게시판 업데이트 메서드
+	@RequestMapping(value = "/board/boardUpadateAction.do", method = RequestMethod.POST)
+	@ResponseBody
+	public String boardUpdateAction(Locale locale,BoardVo boardVo, Model model) throws Exception{
+		
+		HashMap<String, String> result = new HashMap<String, String>();
+		CommonUtil commonUtil = new CommonUtil();
+		
+		int updateRes = boardService.boardUpdate(boardVo);
+		result.put("success", (updateRes > 0)?"Y":"N");
+		String callbackMsg = commonUtil.getJsonCallBackString(" ",result);
+		
+		System.out.println("callbackMsg::"+callbackMsg);
+		
+		return callbackMsg;
+	}
+}
